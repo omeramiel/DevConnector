7eab1794e9873591754d6feaf2b23cfcd876c01c
Admin <omer.amiel@assaabloy.com>
2019-07-14 11:40:22 +0300

    New Components (Dashboard, CreateProfile, EditProfile, Spinner)

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,23 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
-// import setAuthToken from './utils/setAuthToken';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-// const token = localStorage.token;
-// if (token) {
-//   setAuthToken(token);
-// }
+const token = localStorage.token;
+if (token) {
+  setAuthToken(token);
+}
 
 const App = () => {
   useEffect(() => {
@@ -34,6 +38,17 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
             </Switch>
           </section>
         </Fragment>
