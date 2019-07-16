21d37a59dbef4b22f97255e3cf35d6d8243e5205
Admin <omer.amiel@assaabloy.com>
2019-07-16 12:39:54 +0300

    Delete user's posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
+const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -145,8 +146,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // todo: Remove posts
-
+    // Remove posts
+    await Post.deleteMany({ user: req.user.id });
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
